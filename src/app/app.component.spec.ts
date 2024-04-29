1cdcd9689bb0f9374093933aed10b645f18bb287
Magdalena Rychlewska <madzia.rychlewska16@gmail.com>
2024-04-29 10:32:55 +0200

    modul3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'mentoring-program-starter-kit' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mentoring-program-starter-kit');
+    expect(app.card.title).toEqual('mentoring-program-starter-kit');
   });
 
   it('should render title', () => {
